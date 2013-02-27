8a20030ce252ca02084670f3376724b8d4a5c8bd
Rene Bentes Pinto <renebentes@yahoo.com.br>
2013-02-27 16:25:35 -0300

    Update form

--- packages/com_faq/admin/views/faq/tmpl/edit_publish.php
+++ packages/com_faq/admin/views/faq/tmpl/edit_publish.php
@@ -9,19 +9,21 @@
 // No direct access.
 defined('_JEXEC') or die;
 
-$fieldSets = $this->form->getFieldsets('publish');
+$fieldSets = $this->form->getFieldsets();
 foreach ($fieldSets as $name => $fieldSet):
-	echo JHtml::_('sliders.panel', JText::_($fieldSet->label), $name . '-options');
-	if (isset($fieldSet->description) && trim($fieldSet->description)):
-		echo '<p class="tip">' . $this->escape(JText::_($fieldSet->description)) . '</p>';
-	endif;
-	?>
-	<fieldset class="panelform">
-		<ul class="adminformlist">
-		<?php foreach ($this->form->getFieldset($name) as $field): ?>
-			<li><?php echo $field->label; ?>
-			<?php echo $field->input; ?></li>
-		<?php endforeach; ?>
-		</ul>
-	</fieldset>
-<?php endforeach;
+	if ($name == 'publish') :
+		echo JHtml::_('sliders.panel', JText::_($fieldSet->label), $name . '-options');
+		if (isset($fieldSet->description) && trim($fieldSet->description)):
+			echo '<p class="tip">' . $this->escape(JText::_($fieldSet->description)) . '</p>';
+		endif;
+		?>
+		<fieldset class="panelform">
+			<ul class="adminformlist">
+			<?php foreach ($this->form->getFieldset($name) as $field): ?>
+				<li><?php echo $field->label; ?>
+				<?php echo $field->input; ?></li>
+			<?php endforeach; ?>
+			</ul>
+		</fieldset>
+	<?php endif;
+endforeach;
